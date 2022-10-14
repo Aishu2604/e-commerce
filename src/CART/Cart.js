bc721c5acefeb70d4a972ab46b73353c392e5f4c
Aishu2604 <aishwary.nigam04@gmail.com>
2022-10-14 14:14:48 +0530

    Cart Component

--- src/CART/Cart.js
+++ src/CART/Cart.js
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Cart.css";
 
-const Cart = () => {
-    return
-}
+const Cart = (props) => {
+  const cartElements = [
+    {
+      id: 1,
+
+      title: "Colors",
+
+      price: 100,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+
+      quantity: 2,
+    },
+
+    {
+      id: 2,
+      title: "Black and white Colors",
+
+      price: 50,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+
+      quantity: 3,
+    },
+
+    {
+      id: 3,
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+
+      quantity: 1,
+    },
+  ];
+
+  let [cartItems, setCartItems] = useState(cartElements);
+
+  if (!props.open) {
+    return;
+  }
+  function removeItem (removeCartItem){
+    setCartItems(
+      () =>
+        (cartItems = cartItems.filter((key) => key.id !== removeCartItem.id))
+    );
+  };
+
+  return (
+    <div className="mainCart">
+      <h1> cart </h1>
+      <button onClick={props.close}> * </button>
+      <div className="cart">
+        <h3> Item</h3>
+        <h2> Price</h2>
+        <h3> Quantity </h3>
+      </div>
+      <div className="cartItem">
+        {cartItems.map((item) => {
+          return (
+            <div className="mapItem">
+              <div className="image">
+                <img src={item.imageUrl} alt=" " width={70} height={70} />
+                <h4>{item.title}</h4>
+              </div>
+              <div className="price">
+                <h3>{item.price}</h3>
+              </div>
+              <div className="input">
+                <input type="number" />
+                <button onClick={() => removeItem(item)}>REMOVE</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button> PURCHASE </button>
+    </div>
+  );
+};
 
 export default Cart;
--- src/CART/CartButton.js
+++ src/CART/CartButton.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import Cart from "./Cart";
 
-const CartButton = () => {
-    const [count, setCount] = useState(0);
-    return(
-        <div>
-            <button>Cart {count}</button>
-        </div>
+const CartButton = (props) => {
+  const [count, setCount] = useState(0);
 
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
+  const openCart = () => {
+    setIsOpen(true);
+  }
+  
+  const hideCart =() => {
+    setIsOpen(false)
+  }
+
+  return (
+    <div>
+      <button onClick={openCart}>Cart {count}</button>
+      <Cart open={isOpen} close={hideCart} />
+    </div>
+  );
+};
 export default CartButton;
